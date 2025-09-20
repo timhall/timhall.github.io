27d6e4c0b6f79df45647d1e0840cadba2b02cfa6
timhall <tim.hall.engr@gmail.com>
2025-09-20 19:04:17 -0400

    feat: continue work on streaming example

--- src/components/articles/streams/readable-stream.svelte
+++ src/components/articles/streams/readable-stream.svelte
@@ -1,68 +1,101 @@
-<script>
-	let controller;
+<script lang="ts">
+	let controller: ReadableStreamDefaultController;
+	let unhandledError: unknown | null = $state(null);
+	const internal: {
+		state: "uninitialized" | "started" | "closed" | "cancelled" | "error";
+		queue: number[];
+		error: unknown | null;
+	} = $state({
+		state: "uninitialized",
+		queue: [],
+		error: null,
+	});
+
 	const stream = new ReadableStream({
 		start(_controller) {
 			controller = _controller;
+			internal.state = "started";
+		},
+		cancel() {
+			internal.state = "cancelled";
 		},
 	});
 
 	let index = 0;
-	let buffer = [];
-	let closed = false;
-	function enqueue() {
-		const value = (index += 1);
-
-		controller.enqueue(value);
-		buffer.push(value);
-		buffer = buffer;
+	function enqueue(value = (index += 1)) {
+		try {
+			controller.enqueue(value);
+			internal.queue.push(value);
+		} catch (error) {
+			unhandledError = error;
+		}
 	}
 	function close() {
-		closed = true;
-		controller.close();
+		try {
+			internal.state = "closed";
+			controller.close();
+		} catch (error) {
+			unhandledError = error;
+		}
+	}
+	function error(error: unknown = new Error("Uh oh.")) {
+		try {
+			internal.state = "error";
+			internal.error = error;
+			controller.error(error);
+		} catch (error) {
+			unhandledError = error;
+		}
 	}
 
+	// Reader
 	const reader = stream.getReader();
-	let results = [];
+	let latest:
+		| { pending: true }
+		| { resolved: { done: false; value: number } | { done: true } }
+		| { rejected: unknown }
+		| null = $state(null);
 
 	async function read() {
-		const result = await reader.read();
-
-		buffer.shift();
-		buffer = buffer;
-
-		results.push(result);
-		results = results;
+		if (latest && ("pending" in latest || "rejected" in latest)) {
+			return;
+		}
+		try {
+			latest = { pending: true };
+			const resolved = await reader.read();
+			internal.queue.shift();
+			latest = { resolved };
+		} catch (error) {
+			latest = { rejected: error };
+		}
+	}
+	function cancel() {
+		try {
+			reader.cancel();
+			internal.queue = [];
+		} catch (error) {
+			unhandledError = error;
+		}
 	}
 </script>
 
-<div>
-	<h3>Controller</h3>
-	<button onclick={enqueue} disabled={closed}>Enqueue</button>
-	<button onclick={close} disabled={closed}>Close</button>
-</div>
-
-<div>
-	<h3>State</h3>
-
-	Closed: {closed}
-
-	Buffer:
-	<ul>
-		{#each buffer as value}
-			<li>{value}</li>
-		{/each}
-	</ul>
-</div>
-
-<div>
-	<h3>Reader</h3>
-
+<div class="readable-stream-example">
+	<button onclick={() => enqueue()}>Enqueue</button>
+	<button onclick={close}>Close</button>
+	<button onclick={cancel}>Cancel</button>
+	<button onclick={() => error()}>Error</button>
 	<button onclick={read}>Read</button>
 
-	Values:
-	<ul>
-		{#each results as result}
-			<li>{JSON.stringify(result)}</li>
-		{/each}
-	</ul>
+	<pre>{JSON.stringify(
+			$state.snapshot({ internal, latest, unhandledError }),
+			(key, value) =>
+				value instanceof Error ? { message: value.message } : value,
+			2,
+		)}</pre>
 </div>
+
+<style>
+	.readable-stream-example {
+		font-family: var(--pico-font-family-monospace);
+	}
+</style>
